5d0252cd6d7c0fa8aad408ac4b0bcb950bf5693d
Your Name <your.email@example.com>
2026-08-12 01:58:55 +0200

    Initial commit: Add project solution files and entry point

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CybersecurityAwarenessBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BotEngine engine = new BotEngine();
+            engine.Start();
+        }
+    }
+}
